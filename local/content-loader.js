90abe0ae226212ced3660fe6431fa726c8f8647d
webmobilidev <elio.rosati@gpchannel.com>
2017-04-12 16:33:41 +0200

    inserito contacts loader

--- local/content-loader.js
+++ local/content-loader.js
@@ -1,16 +1,22 @@
 function ContentLoader(){
   var constructor = function(){
     setInterval(function(){checkValidation(navigator.connection.type);},30000);
+    loadShopInfo();
   }
   
   function checkValidation(connection){
     if((connection != "none" && connection != "unknown")){
       console.log("refreshato");
-      window.location.reload(true);
-    }
-    
+      window.location.href = 'index.html';    
+    }  
   }
-  
+  function loadShopInfo(){
+    $.get('shop/contacts.html', function(data){
+      if($(data).length > 0){
+        $(data).appendTo($('#mainLogo'))
+      }    
+    });
+  }
   constructor();
 }
 (function () {
