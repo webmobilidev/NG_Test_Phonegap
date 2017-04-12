56c8954ac12584daf6eaef01b43428f0242765a7
webmobilidev <elio.rosati@gpchannel.com>
2017-04-12 15:46:10 +0200

    content loader inserito

---
+++ local/content-loader.js
@@ -0,0 +1,15 @@
+function ContentLoader(){
+  var constructor = function(){
+    setInterval(checkValidation(navigator.connection.type),30000);
+  }
+  
+  function checkValidation(connection){
+    if((connection != "none" || connection != "unknown")){
+      console.log("refreshato");
+      window.location.reload(true);
+    }
+    
+  }
+  
+  constructor();
+}
